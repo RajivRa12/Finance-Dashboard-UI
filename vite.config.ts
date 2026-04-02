11fcebe6d755a7db1ca148edb441371c12b0563b
Palla Rajiv <RajivRa12@users.noreply.github.com>
2026-04-03 01:03:27 +0530

    Serve static site from GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Finance-Dashboard-UI/',
+  base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'site-entry.html',
+    },
+  },
 })
